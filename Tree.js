7e0d44b7d3f29575fac4133b6f5f6ba0ae83f27a
Spechtenhauser Florian <f.spechtenhauser@gmail.com>
2012-04-04 11:45:21 +0200

    added weight to a node to store the weight of the edge from the current node to its parent; added comments

--- Tree.js
+++ Tree.js
@@ -1,17 +1,32 @@
 function Vis2Node()
 {
+	//Name of the node (e.g. "A")
 	this.name;
+	
+	//Parent of the node
 	this.parent;
+	
+	//Weight of the edge between parent and this node
+	this.edgeweight;
+	
+	//Array of the children
 	this.children = new Array();
+	
+	//Variable/function to access PushChild function
 	this.AddChild = PushChild;
+	
+	//Variable/function to access GetChildrenCount function
 	this.numChildren = GetChildrenCount;
 	
-	function PushChild(child)
+	//Adds a child to this node
+	function PushChild(child, weight)
 	{
 		this.children.push(child);
 		child.parent = this;
+		child.edgeweight = weight;
 	}
 	
+	//Returns the number of children of this node
 	function GetChildrenCount()
 	{
 		return this.children.length;
