47264470ba2374ecbff0ee5cb454381a9315bf89
Spechtenhauser Florian <f.spechtenhauser@gmail.com>
2012-05-08 11:47:10 +0200

    removed outline movement of splitter, because currently we dont have performance issues

--- Vis2Splitter.js
+++ Vis2Splitter.js
@@ -3,21 +3,21 @@ $().ready(function() {
 	// First horizontal splitters, nested in the right pane of the vertical splitter.
 	$("#LeftPaneSplitter1").splitter({
 		splitHorizontal: true,
-		outline: true,
+		//outline: true,
 		sizeTop: 100, minTop: 50, maxTop: 200,
 		accessKey: "V"
 	});
 	// Second horizontal splitter, nested in bottom pane of first horizontal splitter
 	$("#LeftPaneSplitter2").splitter({
 		splitHorizontal: true,
-		outline: true,
+		//outline: true,
 		//sizeBottom: 120, minTop: 50,
 		accessKey: "J"
 	});
 	// Vertical splitter. Set min/max/starting sizes for the left pane.
 	$("#MainWindow").splitter({
 		splitVertical: true,
-		outline: true,
+		//outline: true,
 		sizeLeft: true,
 		anchorToWindow: true,
 		accessKey: "L"
