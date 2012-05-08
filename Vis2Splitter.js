8a0cd1dd980866aded17ce091ab649e74d76dc5f
Spechtenhauser Florian <f.spechtenhauser@gmail.com>
2012-05-08 11:46:12 +0200

    divided window in 4 panes for the interface we want at the end

--- Vis2Splitter.js
+++ Vis2Splitter.js
@@ -1,7 +1,26 @@
 $().ready(function() {
-	$("#MySplitter").splitter({
-		type: "v", 
-		sizeLeft: true,	/* use height set in stylesheet */
-		accessKey: "P"
+	
+	// First horizontal splitters, nested in the right pane of the vertical splitter.
+	$("#LeftPaneSplitter1").splitter({
+		splitHorizontal: true,
+		outline: true,
+		sizeTop: 100, minTop: 50, maxTop: 200,
+		accessKey: "V"
 	});
+	// Second horizontal splitter, nested in bottom pane of first horizontal splitter
+	$("#LeftPaneSplitter2").splitter({
+		splitHorizontal: true,
+		outline: true,
+		//sizeBottom: 120, minTop: 50,
+		accessKey: "J"
+	});
+	// Vertical splitter. Set min/max/starting sizes for the left pane.
+	$("#MainWindow").splitter({
+		splitVertical: true,
+		outline: true,
+		sizeLeft: true,
+		anchorToWindow: true,
+		accessKey: "L"
+	});
+
 });
