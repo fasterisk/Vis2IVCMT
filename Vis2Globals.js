f5dba8545437d6d6aed1d3c2a015252cdf177598
Spechtenhauser Florian <f.spechtenhauser@gmail.com>
2012-05-08 14:21:41 +0200

    moved viewManager, measureevent and colorevents to globals

--- Vis2Globals.js
+++ Vis2Globals.js
@@ -1,5 +1,6 @@
 Debugger.log("Adding Window-Eventlistener");
 window.addEventListener('load', eventWindowLoaded, false);
+window.viewManager = new ViewManager();
 
 function eventWindowLoaded()
 {
@@ -8,5 +9,45 @@ function eventWindowLoaded()
 		Debugger.log("Canvas is not supported!");
 		return;
 	}
-	RightPaneCanvas();
+	window.rightPaneCanvas = new RightPaneCanvas();
+	
+	var formElement = document.getElementById("measure");
+	formElement.addEventListener('change', measureChanged, false);
+
+	formElement = document.getElementById("color1");
+	formElement.addEventListener('change', color1Changed, false);
+
+	formElement = document.getElementById("color2");
+	formElement.addEventListener('change', color2Changed, false);
+
+	formElement = document.getElementById("MainWindow");
+	formElement.addEventListener('mouseup', canvasSizeChanged, false);
+}
+
+function measureChanged(e)
+{
+	Debugger.log("Measure changed.");
+	var target = e.target;
+	window.viewManager.ChangeMeasure(target.value);
+	window.rightPaneCanvas.DrawScreen();
+}
+
+function color1Changed(e)
+{
+	Debugger.log("Color 1 changed.");
+	var target = e.target;
+	window.viewManager.ChangeColorMap('#' + target.value, window.viewManager.color9);
+	window.rightPaneCanvas.DrawScreen();
+}
+function color2Changed(e)
+{
+	Debugger.log("Color 2 changed.");
+	var target = e.target;
+	window.viewManager.ChangeColorMap(window.viewManager.color1, '#' + target.value);
+	window.rightPaneCanvas.DrawScreen();
+}
+function canvasSizeChanged(e)
+{
+	Debugger.log("Canvas resized");
+	window.rightPaneCanvas.DrawScreen();
 }
--- Vis2RightPaneCanvas.js
+++ Vis2RightPaneCanvas.js
@@ -1,22 +1,14 @@
 function RightPaneCanvas()
 {
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
 	
-	var viewmanager = new ViewManager();
 	
-	var formElement = document.getElementById("measure");
-	formElement.addEventListener('change', measureChanged, false);
-
-	formElement = document.getElementById("color1");
-	formElement.addEventListener('change', color1Changed, false);
-
-	formElement = document.getElementById("color2");
-	formElement.addEventListener('change', color2Changed, false);
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");
 	
-	formElement = document.getElementById("MainWindow");
-	formElement.addEventListener('mouseup', canvasSizeChanged, false);
-	
+	/*
+	 * Functions for public access
+	 */
+	this.DrawScreen = M_DrawScreen;
 	
 	var sTestTreeCompact4 = '[1;[2;[3;"A"][4;"B"]][5;"C"]][4;[3;"D"][2;[3;[4;"E"][5;"F"]][6;"G"]]]';
 	var sTestTreeCompact5 = '[2;[2;[2;"B"][2;"D"]][2;"C"]][2;[2;"E"][2;[2;[2;"A"][2;"G"]][2;"F"]]]';
@@ -34,9 +26,9 @@ function RightPaneCanvas()
 	tree2.BuildNeededSpace();
 	tree3.BuildNeededSpace();
 
-	drawScreen();
+	M_DrawScreen();
 
-	function drawScreen()
+	function M_DrawScreen()
 	{
 		Debugger.log("Drawing RightPaneCanvas");
 
@@ -46,7 +38,7 @@ function RightPaneCanvas()
 		context.fillStyle = '#aaaaaa';
 		context.fillRect(0, 0, canvas.width, canvas.height);
 
-		viewmanager.DisplayColorMap(context);
+		window.viewManager.DisplayColorMap(context);
 
 		context.fillStyle = '#000000';
 		tree1.Draw(context, 200, 10);
@@ -55,36 +47,6 @@ function RightPaneCanvas()
 
 		context.textFillColor = "#000000";
 		context.font = "24px sans-serif";
-		context.fillText(viewmanager.measure, 400, 300);
-
-	}
-
-	function measureChanged(e)
-	{
-		Debugger.log("Measure changed.");
-		var target = e.target;
-		viewmanager.ChangeMeasure(target.value);
-		drawScreen();
-	}
-
-	function color1Changed(e)
-	{
-		Debugger.log("Color 1 changed.");
-		var target = e.target;
-		viewmanager.ChangeColorMap('#' + target.value, viewmanager.color9);
-		drawScreen();
-	}
-	function color2Changed(e)
-	{
-		Debugger.log("Color 2 changed.");
-		Debugger.log(canvasPane.offsetWidth);
-		var target = e.target;
-		viewmanager.ChangeColorMap(viewmanager.color1, '#' + target.value);
-		drawScreen();
-	}
-	function canvasSizeChanged(e)
-	{
-		Debugger.log("Canvas resized");
-		drawScreen();
+		context.fillText(window.viewManager.measure, 400, 300);
 	}
 }
