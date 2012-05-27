9fcf879cbd78cfddebd742325fd0b1942da04967
Christian Möllinger <ch.moellinger@gmail.com>
2012-05-27 18:09:36 +0200

    Added some test stuff to create new windows on the fly - not working yet

--- Vis2ViewManager.js
+++ Vis2ViewManager.js
@@ -4,6 +4,8 @@ function Vis2ViewManager()
 	var ReferenceTreeView = undefined;
 	var TreeComparisonView = undefined;
 	
+	var nNextWindowNumber = 1;
+	
 	this.InitializeViews = function()
 	{
 		ComparisonOverview = new Vis2ComparisonOverview("ComparisonOverviewPane");
@@ -18,6 +20,38 @@ function Vis2ViewManager()
 		ComparisonOverview.Update();			
 		ReferenceTreeView.Update();
 		TreeComparisonView.Update();
+	}
+	
+	this.CreateNewComparisonView = function()
+	{
+		var nWindowNumber = nNextWindowNumber;
+		
+		nNextWindowNumber = nNextWindowNumber + 1;
+		
+		var sNewWindowName = "window" + nWindowNumber;
+		
+		var sWindowDivContent = '<div><form>View ' + nWindowNumber + ', select measure:' + 
+										'<select id="measure">'+
+											'<option value="leafbased">leaf-based</option>'+
+											'<option value="elementbased">element-based</option>'+
+											'<option value="edgebased">edge-based</option>'+
+										'</select>'+
+									'</form>'+
+								'</div>' +
+								'<div id="TestPane"' + nWindowNumber + ' style="width: 100%; height: 100%;">' +
+									'<canvas>'+
+										'not simply use HTML5 Canvas with this browser'+
+									'</canvas>'+
+								'</div>';
+		
+		//$("DockingBox").append('<p> HALLLLLLLLLLLO</p>');
+		var items1 = $("#" + sNewWindowName);
+		var items2 = $("#DockingBox");
+								
+		$("#DockingBox").append('<div><div id="' + sNewWindowName + '">' + sWindowDivContent + '</div></div>');
+		
+		$("#" + sNewWindowName).jqxWindow();
+		$("#DockingBox").jqxDocking('addWindow', sNewWindowName, 'docked');
 	}
 	
 }
