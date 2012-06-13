347daaaebf1f0a9dc63d8133fe13d365c9734559
Christian Möllinger <ch.moellinger@gmail.com>
2012-06-13 23:34:25 +0200

    Changed default sizes

--- Views/Vis2ReferenceTreeView.js
+++ Views/Vis2ReferenceTreeView.js
@@ -1,17 +1,19 @@
-function Vis2ReferenceTreeView(divID) {
+function Vis2ReferenceTreeView(divID)
+{
 	/*
-	* Private members
-	*/
+	 * Private members
+	 */
 
 	// canvas associated to this view
 	var DivElement = window.document.getElementById(divID);
 	var CanvasElement = GetCanvasWithinDiv(divID);
 
 	var rTreeVisualizer = undefined;
-	
+
 	var aRenderedNodes = new Array();
 
-	function ItlUpdate() {
+	function ItlUpdate()
+	{
 		// set canvas size to div size
 		CanvasElement.width = DivElement.offsetWidth;
 		CanvasElement.height = DivElement.offsetHeight - 30;
@@ -22,26 +24,31 @@ function Vis2ReferenceTreeView(divID) {
 		// get reference tree
 		ReferenceTree = window.SelectionManager.GetReferenceTree();
 
-		if(ReferenceTree != undefined) {
-			// create visualizer, if undefined or for wrong tree (selected tree may have changed)
-			if(rTreeVisualizer == undefined || rTreeVisualizer.GetNode() != ReferenceTree) {
+		if (ReferenceTree != undefined)
+		{
+			// create visualizer, if undefined or for wrong tree (selected tree
+			// may have changed)
+			if (rTreeVisualizer == undefined
+					|| rTreeVisualizer.GetNode() != ReferenceTree)
+			{
 				rTreeVisualizer = new Vis2NodeVisualizer(ReferenceTree);
 			}
 
-			CanvasElement.height = rTreeVisualizer.GetHeightNeeded();
-						
+			CanvasElement.height = rTreeVisualizer.GetHeightNeeded() + 20;
+
 			// call visualizer
-			aRenderedNodes = rTreeVisualizer.Draw(context, window.TreeManager.GetGlobalMeasure() + 'average', CanvasElement.width / 2, 20, true);
-			
+			aRenderedNodes = rTreeVisualizer.Draw(context, window.TreeManager
+					.GetGlobalMeasure()
+					+ 'average', CanvasElement.width / 2, 20, true);
+
 			Debugger.log("rendered " + aRenderedNodes.length + " nodes");
 		}
-
 
 	}
 
 	/*
-	* code executed while construction
-	*/
+	 * code executed while construction
+	 */
 
 	// attach event handler
 	CanvasElement.addEventListener('mousedown', OnMouseDown, false);
@@ -49,37 +56,40 @@ function Vis2ReferenceTreeView(divID) {
 	CanvasElement.addEventListener('mouseup', OnMouseUp, false);
 
 	// event handler
-	function OnMouseDown(event) {
-		
+	function OnMouseDown(event)
+	{
+
 		// get coordinates
 		var nX = event.offsetX;
 		var nY = event.offsetY;
-		
-		for (var i=0; i < aRenderedNodes.length; i++)
+
+		for ( var i = 0; i < aRenderedNodes.length; i++)
 		{
-			var nDiffX =Math.abs(nX - aRenderedNodes[i].x);
-			var nDiffY =Math.abs(nY - aRenderedNodes[i].y);
-			
+			var nDiffX = Math.abs(nX - aRenderedNodes[i].x);
+			var nDiffY = Math.abs(nY - aRenderedNodes[i].y);
+
 			var fDiff = Math.sqrt(nDiffX * nDiffX + nDiffY * nDiffY);
-			
-			
+
 			var fNodeRadius = aRenderedNodes[i].radius;
-			
+
 			if (fDiff < fNodeRadius)
 			{
 				// node selected!
-								
+
 				// set reference node
-				window.SelectionManager.SetSelectedNode(aRenderedNodes[i].rNode);
+				window.SelectionManager
+						.SetSelectedNode(aRenderedNodes[i].rNode);
 			}
 		}
 	}
 
-	function OnMouseMove(event) {
+	function OnMouseMove(event)
+	{
 		// do nothing
 	}
 
-	function OnMouseUp(event) {
+	function OnMouseUp(event)
+	{
 		// do nothing
 	}
 
